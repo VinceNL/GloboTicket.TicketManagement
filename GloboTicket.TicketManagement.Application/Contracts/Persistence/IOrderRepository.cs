68004f7734c31edae5984e16ae2dafa02100383e
vvancampen <vvancampen@msn.com>
2023-10-10 21:48:37 +0200

    Add project files.

---
+++ GloboTicket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistence
+{
+    internal interface IOrderRepository : IAsyncRepository<Order>
+    {
+    }
+}
